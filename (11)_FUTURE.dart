22759a08668550ac0982ac274badf988057afe36
VICTOR MANUEL ITURBIDE MARIANO <iturbide.mariano.victor.manuel@gmail.com>
2026-03-02 02:29:40 -0600

    DECIMO PRIMER  ARCHIVO FUTURE (PROMISE DE JS) EN DART

---
+++ (11)_FUTURE.dart
@@ -0,0 +1,23 @@
+void main() {
+  print('Inicio del programa');
+  
+  //print(httpGet());
+  
+  httpGet().then((value){/*añadir dentro de httpGet la url cuando exista api*/
+    print(value);
+  }).catchError((err){
+    print('Error: $err');
+  });
+(11)_FUTURE
+solo almacena 
+  
+  print('Fin del programa');
+}
+
+Future<String> httpGet(){/*añadir String url dentro de httpGet()*/
+  return  Future.delayed(const Duration(seconds:3), (){
+    throw 'Error en la petición http';
+    //return "Respuesra de la peticion http";
+  }); //Equivalente al promise de JS indica que tendra un valor a futuro
+  
+}
